e88106fc98f1ee397c6aab4135b974ed2d22b8f2
linjun <1045735402@qq.com>
2019-02-25 13:41:12 +0800

    Add common method in emqx_ct_helpers

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ PROJECT = emqx_ct_helpers
 PROJECT_DESCRIPTION = EMQ X Common Test Helpers
 PROJECT_VERSION = 0.1.1
 
-DEPS = meck
+DEPS = meck proper
 dep_meck = git https://github.com/emqx/meck.git
+dep_proper = git https://github.com/proper-testing/proper.git
 
 BUILD_DEPS = emqx
 dep_emqx = git https://github.com/emqtt/emqttd emqx30
--- src/emqx_ct_helpers.erl
+++ src/emqx_ct_helpers.erl
@@ -18,11 +18,66 @@
 
 -include_lib("common_test/include/ct.hrl").
 
--export([ensure_broker_started/0, ensure_broker_stopped/0]).
+-export([set_config/1,run_setup_steps/1, run_teardown_steps/0, reload/2, ensure_broker_started/0, ensure_broker_stopped/0]).
 
 ensure_broker_started() ->
-    {ok, _} = emqx_ct_broker:start_link(), ok.
+    {ok, _} = emqx_ct_broker:start_link(),  
+    ok.
 
 ensure_broker_stopped() ->
     emqx_ct_broker:stop().
 
+set_config(Config) when is_list(Config) ->
+    set_config(Config, []).
+
+set_config([{App, SchemaPath, ConfPath} | ConfigInfo], Acc) ->
+    set_config(ConfigInfo, [{App, local_path(SchemaPath), local_path(ConfPath)} | Acc]);
+set_config([], Acc) ->
+    Acc.
+local_path(RelativePath) ->
+    filename:join([get_base_dir(), RelativePath]).
+
+get_base_dir() ->
+    {file, Here} = code:is_loaded(?MODULE),
+    filename:dirname(filename:dirname(Here)).
+
+run_setup_steps(Config)when is_list(Config) ->
+    [start_apps(App, {SchemaFile, ConfigFile}) || {App, SchemaFile, ConfigFile} <- Config].
+
+run_teardown_steps() ->
+    [application:stop(App) || {_, App} <- erlang:erase()],
+    ekka_mnesia:stop().
+
+start_apps(App, {SchemaFile, ConfigFile}) ->
+    erlang:erase(),
+    erlang:put(App, App),
+    read_schema_configs(App, {SchemaFile, ConfigFile}),
+    set_special_configs(App),
+    application:ensure_all_started(App).
+
+read_schema_configs(App, {SchemaFile, ConfigFile}) ->
+    ct:pal("Read configs - SchemaFile: ~p, ConfigFile: ~p", [SchemaFile, ConfigFile]),
+    Schema = cuttlefish_schema:files([SchemaFile]),
+    Conf = conf_parse:file(ConfigFile),
+    NewConfig = cuttlefish_generator:map(Schema, Conf),
+    Vals = proplists:get_value(App, NewConfig, []),
+    [application:set_env(App, Par, Value) || {Par, Value} <- Vals].
+
+set_special_configs(emqx) ->
+    application:set_env(emqx, allow_anonymous, false),
+    application:set_env(emqx, enable_acl_cache, false);
+set_special_configs(_App) ->
+    ok.
+
+reload(APP, {Par, Vals}) when is_atom(APP), is_list(Vals) ->
+    application:stop(APP),
+    {ok, TupleVals} = application:get_env(APP, Par),
+    NewVals =
+    lists:filtermap(fun({K, V}) ->
+        case lists:keymember(K, 1, Vals) of
+        false ->{true, {K, V}};
+        _ -> false
+        end
+    end, TupleVals),
+    application:set_env(APP, Par, lists:append(NewVals, Vals)),
+    application:start(APP).
