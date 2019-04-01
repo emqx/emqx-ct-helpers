9652e13585bab6a82f0e93c4f711010810afd86a
YoukiLin <1045735402@qq.com>
2019-04-01 13:35:52 +0800

    Update emqx_ct_helpers.erl

--- src/emqx_ct_helpers.erl
+++ src/emqx_ct_helpers.erl
@@ -19,11 +19,11 @@
 -include_lib("common_test/include/ct.hrl").
 
 -export([ set_config/1
-         , run_setup_steps/2
-         , reload/2
-         , start_apps/1
-         , start_apps/2
-         , stop_apps/1
+        , run_setup_steps/2
+        , reload/2
+        , start_apps/1
+        , start_apps/2
+        , stop_apps/1
         ]).
 
 set_config(Config) when is_list(Config) ->
